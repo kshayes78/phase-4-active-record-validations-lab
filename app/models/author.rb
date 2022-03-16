f7a9705f0ccc1f18f267d5d360c1cc0332bf3d9c
kshayes78 <89166628+kshayes78@users.noreply.github.com>
2022-03-15 22:16:37 -0400

    commit tree bb102d3e8bc0922435b939e8d04a299cdb1c1e05

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+     #Author's name can't be empty/falsy
+    validates :name, presence: true
+     #name is case-sensitively unique
+    validates :name, uniqueness: {case_sensitive: true}
+    #length of phone# is 10
+    validates :phone_number, length: {is: 10}
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+    #post's title can't be empty/falsy
+    validates :title, presence: true 
+   # Post Basic validations is expected to validate that the length of :content is at least 250
+    validates :content, length: {minimum: 250}
+#    # Post Basic validations is expected to validate that the length of :summary is at most 250 
+    validates :summary, length: {maximum: 250}
+#     # Post Basic validations is expected to validate that :category is either ‹"Fiction"› or ‹"Non-Fiction"›
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+#     # Post Custom validations: title is clickbait is expected not to allow :title to be ‹"True Facts"›
+    validate :clickbait?
+    
+#     # add a custom validator that ensures the title is clickbait-y, should add a validation error if the title does not contain:
+
+CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
